77f29848b8e3c37f4fa1413b667d6bec9a846d51
Nicolas Grenié <nicolas.grenie@gmail.com>
2018-02-15 12:29:29 -0800

    add openapi spec

--- server.js
+++ server.js
@@ -21,14 +21,18 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+router.get('/hello', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/', router);
 
 // START THE SERVER
 // =============================================================================
