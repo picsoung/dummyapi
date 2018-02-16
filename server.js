d9fe486a67f6633ce63d37c6d21c7a67c326e000
Nicolas Grenié <nicolas.grenie@gmail.com>
2018-02-16 14:51:17 -0800

    test gh api subscirbe events

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+// add comment
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
